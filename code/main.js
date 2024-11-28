1a52d2b27079f4d91f7f623d85d1b155839c8aa4
Vinicius Lima <thelimavinicius@gmail.com>
2024-11-28 14:41:02 -0300

    save to csv

--- code/main.js
+++ code/main.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const fs = require('fs');
 
 const GITHUB_TOKEN = '';
 const BASE_URL = 'https://api.github.com';
@@ -7,14 +8,14 @@ const REPOS_LENGTH = 2;
 const CALL_REPS = 2;
 const REPOS_INFO_LENGTH = 1;
 
-
 axios.defaults.headers.common['Authorization'] = `Bearer ${GITHUB_TOKEN}`;
-
 
 function median(arr) {
   const sorted = [...arr].sort((a, b) => a - b);
   const mid = Math.floor(sorted.length / 2);
-  return sorted.length % 2 !== 0 ? sorted[mid] : (sorted[mid - 1] + sorted[mid]) / 2;
+  return sorted.length % 2 !== 0
+    ? sorted[mid]
+    : (sorted[mid - 1] + sorted[mid]) / 2;
 }
 
 function average(arr) {
@@ -42,7 +43,6 @@ async function fetchTopReposGraphQL() {
   let responseSize;
 
   for (let i = 0; i < CALL_REPS; i++) {
-    console.time(`GraphQL API Call ${i + 1}`);
     const start = Date.now();
     const response = await axios.post(GRAPHQL_URL, {
       query: `
@@ -62,7 +62,6 @@ async function fetchTopReposGraphQL() {
           `,
     });
     const end = Date.now();
-    console.timeEnd(`GraphQL API Call ${i + 1}`);
     times.push(end - start);
     responseSize = JSON.stringify(response.data).length;
   }
@@ -70,18 +69,15 @@ async function fetchTopReposGraphQL() {
   return { medianTime: median(times), avgTime: average(times), responseSize };
 }
 
-
 async function fetchRepoDetailsREST(owner, repo) {
   const repoUrl = `${BASE_URL}/repos/${owner}/${repo}`;
   const times = [];
   let responseSize;
 
   for (let i = 0; i < CALL_REPS; i++) {
-    console.time(`REST Repo Detail Call ${i + 1}`);
     const start = Date.now();
     const repoResponse = await axios.get(repoUrl);
     const end = Date.now();
-    console.timeEnd(`REST Repo Detail Call ${i + 1}`);
     times.push(end - start);
     responseSize = JSON.stringify(repoResponse.data).length;
   }
@@ -110,11 +106,9 @@ async function fetchRepoDetailsGraphQL(owner, repo) {
   let responseSize;
 
   for (let i = 0; i < CALL_REPS; i++) {
-    console.time(`GraphQL Repo Detail Call ${i + 1}`);
     const start = Date.now();
     const response = await axios.post(GRAPHQL_URL, query);
     const end = Date.now();
-    console.timeEnd(`GraphQL Repo Detail Call ${i + 1}`);
     times.push(end - start);
     responseSize = JSON.stringify(response.data).length;
   }
@@ -122,26 +116,107 @@ async function fetchRepoDetailsGraphQL(owner, repo) {
   return { medianTime: median(times), avgTime: average(times), responseSize };
 }
 
+function createAndWriteCSV(filename, header) {
+  fs.writeFileSync(filename, `${header}\n`);
+}
+
+function appendToCSV(data, filename) {
+  const csvData = `${data.join(',')}\n`;
+  fs.appendFileSync(filename, csvData);
+}
 
 async function main() {
-  // Passo 1 Pegar os 100 repositórios mais famosos
+  const passo1Filename = 'passo1_metrics.csv';
+  const passo2Filename = 'passo2_metrics.csv';
+
+  // Write headers for each CSV
+  createAndWriteCSV(
+    passo1Filename,
+    'Step,API,MedianTime,AverageTime,ResponseSize'
+  );
+  createAndWriteCSV(
+    passo2Filename,
+    'Step,API,MedianTime,AverageTime,ResponseSize'
+  );
+
   console.log(`Fetching top ${REPOS_LENGTH} repositories...`);
   const { medianTime, avgTime, responseSize } = await fetchTopReposREST();
-  const { medianTime: medianTimeGraphQl, avgTime: avgTimeGraphQL, responseSize: responseSizeGraphQL } = await fetchTopReposGraphQL();
-  console.log(`REST API Median Time: ${medianTime}ms, Average Time: ${avgTime}ms, Response Size: ${responseSize} bytes`);
-  console.log(`GraphQL API Median Time: ${medianTimeGraphQl}ms, Average Time: ${avgTimeGraphQL}ms, Response Size: ${responseSizeGraphQL} bytes`);
+  appendToCSV(
+    ['Passo 1', 'REST', medianTime, avgTime, responseSize],
+    passo1Filename
+  );
 
-  const topRepos = await axios.get(`${BASE_URL}/search/repositories?q=stars:%3E1&sort=stars&order=desc&per_page=${REPOS_LENGTH}`);
-  const randomRepos = topRepos.data.items.slice(0, REPOS_INFO_LENGTH); // Select 10 random repos
+  const {
+    medianTime: medianTimeGraphQl,
+    avgTime: avgTimeGraphQL,
+    responseSize: responseSizeGraphQL,
+  } = await fetchTopReposGraphQL();
+  appendToCSV(
+    [
+      'Passo 1',
+      'GraphQL',
+      medianTimeGraphQl,
+      avgTimeGraphQL,
+      responseSizeGraphQL,
+    ],
+    passo1Filename
+  );
+
+  console.log(
+    `REST API Median Time: ${medianTime}ms, Average Time: ${avgTime}ms, Response Size: ${responseSize} bytes`
+  );
+  console.log(
+    `GraphQL API Median Time: ${medianTimeGraphQl}ms, Average Time: ${avgTimeGraphQL}ms, Response Size: ${responseSizeGraphQL} bytes`
+  );
+
+  const topRepos = await axios.get(
+    `${BASE_URL}/search/repositories?q=stars:%3E1&sort=stars&order=desc&per_page=${REPOS_LENGTH}`
+  );
+  const randomRepos = topRepos.data.items.slice(0, REPOS_INFO_LENGTH);
   for (const repo of randomRepos) {
     const { owner, name } = repo;
     console.log(`\nFetching details for repository: ${name}`);
-    const { medianTime: medianTimeRestRepo, avgTime: avgTimeRestRepo, responseSize: responseTimeRestRepo } = await fetchRepoDetailsREST(owner.login, name);
-    const { medianTime: medianTimeGraphQlRepo, avgTime: avgTimeGraphQLRepo, responseSize: responseSizeGraphQLRepo } = await fetchRepoDetailsGraphQL(owner.login, name);
-    console.log(`REST API Median Time: ${medianTimeRestRepo}ms, Average Time: ${avgTimeRestRepo}ms, Response Size: ${responseTimeRestRepo} bytes`);
-    console.log(`GraphQL API Median Time: ${medianTimeGraphQlRepo}ms, Average Time: ${avgTimeGraphQLRepo}ms, Response Size: ${responseSizeGraphQLRepo} bytes`);
+    const {
+      medianTime: medianTimeRestRepo,
+      avgTime: avgTimeRestRepo,
+      responseSize: responseTimeRestRepo,
+    } = await fetchRepoDetailsREST(owner.login, name);
+    appendToCSV(
+      [
+        'Passo 2',
+        'REST',
+        medianTimeRestRepo,
+        avgTimeRestRepo,
+        responseTimeRestRepo,
+      ],
+      passo2Filename
+    );
+
+    const {
+      medianTime: medianTimeGraphQlRepo,
+      avgTime: avgTimeGraphQLRepo,
+      responseSize: responseSizeGraphQLRepo,
+    } = await fetchRepoDetailsGraphQL(owner.login, name);
+    appendToCSV(
+      [
+        'Passo 2',
+        'GraphQL',
+        medianTimeGraphQlRepo,
+        avgTimeGraphQLRepo,
+        responseSizeGraphQLRepo,
+      ],
+      passo2Filename
+    );
+
+    console.log(
+      `REST API Median Time: ${medianTimeRestRepo}ms, Average Time: ${avgTimeRestRepo}ms, Response Size: ${responseTimeRestRepo} bytes`
+    );
+    console.log(
+      `GraphQL API Median Time: ${medianTimeGraphQlRepo}ms, Average Time: ${avgTimeGraphQLRepo}ms, Response Size: ${responseSizeGraphQLRepo} bytes`
+    );
   }
 
+  console.log(`Metrics saved to ${passo1Filename} and ${passo2Filename}`);
 }
 
 main();
